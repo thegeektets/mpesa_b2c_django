316416c9588506f06b41c47e74c7c2064aa33f9a
Unknown <unknown@example.com>
2017-06-09 17:02:38 +0300

    assignee button now works

--- Depreciation/admin.py
+++ Depreciation/admin.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 
 from .models import Assets
 from .models import Depreciation
-from .models import User
 
-admin.site.register(User)
 admin.site.register(Assets)
 admin.site.register(Depreciation)
--- Depreciation/models.py
+++ Depreciation/models.py
@@ -1,10 +1,6 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
-class User(AbstractUser):
-
 
 
 class Depreciation (models.Model):
