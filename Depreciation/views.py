358540e326f1d59365bac9f00f271f6e845529be
markusmash <markusmacharia@gmail.com>
2017-06-19 13:29:11 +0300

    created django rest-framework

--- Depreciation/views.py
+++ Depreciation/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Depreciation
-from .models import Assets
-from .models import Users
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from Depreciation.models import Assets, Depreciation
+from Depreciation.serializers import UserSerializer, AssetsSerializer, DepreciationSerializer
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class DepreciationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Depreciation.objects.all()
+    serializer_class = DepreciationSerializer
+
+
+class AssetsViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Assets.objects.all()
+    serializer_class = AssetsSerializer
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
 
+from Depreciation.views import UserViewSet, AssetsViewSet, DepreciationViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'users', UserViewSet, base_name='users')
+router.register(r'assets', AssetsViewSet, base_name='assets')
+router.register(r'depreciation',DepreciationViewSet, base_name='depreciation')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls)),
 ]
