0eed1088d24febee04b05e5dfe56e4c9405ec60a
markusmash <markusmacharia@gmail.com>
2017-06-19 14:05:58 +0300

    Revert "created DRF urls"
    This reverts commit 83616d8361dd2fbb6d77f5fb63df81340c8619bd.

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'Depreciation',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
