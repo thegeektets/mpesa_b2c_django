1c14f4bd621f2ceeedfc0ad25753f32a7fda5327
Maureen Mumbi <mumbigithinji96@gmail.com>
2017-06-22 16:04:56 +0300

    added first and last name fields to user serializer

--- Depreciation/serializers.py
+++ Depreciation/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username','email']
+        fields = ['id','username','email', 'first_name', 'last_name']
 
 
 class AssetsSerializer(serializers.ModelSerializer):
