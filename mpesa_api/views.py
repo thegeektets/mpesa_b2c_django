52af8fc75b1fa6ba90519d6d720faf2af0decc0b
griffin <griffin@tunaweza.com>
2017-08-14 13:13:53 +0300

    added custom class

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -4,15 +4,29 @@ from rest_framework import viewsets
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
 
 from mpesa_api.models import MpesaLog
 from mpesa_api.request import OAuth, B2C
 from mpesa_api.serializers import MpesaLogSerializer
 from rest_framework import status
 
+class IsCreationOrIsAuthenticated(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated():
+            if view.action == 'create':
+                return True
+            else:
+                return False
+        else:
+            return True
+
 class MpesaAPIViewSet(viewsets.ModelViewSet):
     queryset = MpesaLog.objects.all()
     serializer_class = MpesaLogSerializer
+    permission_classes = (IsCreationOrIsAuthenticated, )
+
 
     def list(self, request, *args, **kwargs):
         b2c = B2C()
