90e63dc404b5f48f900a36d684bf5559d67a8600
griffin <griffin@tunaweza.com>
2017-08-15 11:53:37 +0300

    response test

--- mpesa_api/request.py
+++ mpesa_api/request.py
@@ -72,7 +72,7 @@ class B2C:
         payload['Remarks'] = remarks
         payload['Occasion'] = occasion
 
-        #response = requests.post(self.api_url, json=payload, headers=self.get_headers())
+        response = requests.post(self.api_url, json=payload, headers=self.get_headers())
         #self.response = json.loads(response.text)
-        return payload
+        return response
 
